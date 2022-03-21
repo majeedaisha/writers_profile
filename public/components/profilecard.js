8511e8ffb9804c38cee62c1b916cecc9678fdfc3
majeedaisha <majeedaisha44@gmail.com>
2022-03-21 08:43:06 -0700

    first commit

---
+++ public/components/profilecard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function 
+() {
+  return (
+    <div className='card'>
+    <img src= { 'images/${writers.avartar} .png'}
+    height="300px" width="300px" alt={writers.img}/>
+    
+    
+    
+    
+    
+    
+    
+    
+    </div>
+  )
+}
+
+export default 
+
